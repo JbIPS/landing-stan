bfe5bf7d4a4ee8fa570547542a0d530929c12bc6
JbIPS <jeanbaptiste.richardet@gmail.com>
2023-09-22 10:53:37 +0200

    Register component in index

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,15 @@
 // https://vitepress.dev/guide/custom-theme
 import Layout from "./Layout.vue";
+import Home from "./Home.vue";
+import APropos from "./APropos.vue";
+import Offre from "./Offre.vue";
 import "./style.css";
 
 export default {
   Layout,
-  enhanceApp({ app, router, siteData }) {},
+  enhanceApp({ app }) {
+    app.component("home", Home);
+    app.component("apropos", APropos);
+    app.component("offre", Offre);
+  },
 };
